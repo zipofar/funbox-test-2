5ca4c0e3752321a1ccd97e230760dbb0c65038c8
ingprog <zipofar@gmail.com>
2018-07-16 16:29:53 +0300

    Fix devtools only for development env

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import thunk from 'redux-thunk';
 import RootReducer from './reducers';
 import App from './components/App';
 
-const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
-const devtoolMiddleware = ext && ext();
 
-const store = createStore(RootReducer, compose(applyMiddleware(thunk), devtoolMiddleware));
+let store;
+if (process.env.NODE_ENV === 'development') {
+    const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
+    const devtoolMiddleware = ext && ext();
+    store = createStore(RootReducer, compose(applyMiddleware(thunk), devtoolMiddleware));
+} else {
+    store = createStore(RootReducer, compose(applyMiddleware(thunk)));
+}
 
 ReactDOM.render(
     <Provider store = { store }>
